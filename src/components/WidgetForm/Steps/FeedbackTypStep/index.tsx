2bdf637ea5f8b2033d23fedd8492c692edbefcb2
Ronald Assis <ronaldassis71@gmail.com>
2022-05-04 12:43:58 -0300

    fix: Structuring Widget components

--- src/components/WidgetForm/Steps/FeedbackTypStep/index.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypStep/index.tsx
@@ -1,5 +1,28 @@
-export function FeedbackTypeStep() {
+import { feedbackTypes, FeedbackType } from '../..';
+import styles from './styles.module.css';
+
+interface FeedbacktypeStepProps {
+  onFeedbackTypeChanged: (type: FeedbackType) => void;
+}
+
+export function FeedbackTypeStep({onFeedbackTypeChanged}: FeedbacktypeStepProps) {
   return (
-    <h1>FeedbackTypeStep</h1>
+      <div className={styles.contentWidgetForm}>
+       {
+         Object.entries(feedbackTypes).map(([key, value], i) => {
+           return (
+            <button
+              key={`${i}_${key}`}
+              className={`${styles.buttonFeedbackTypes} bg-zinc-800`}
+              type="button"
+              onClick={() => onFeedbackTypeChanged(key as FeedbackType)}
+            >
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+           )
+         })
+       }
+      </div>
   )
 }
--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,7 +1,42 @@
-import styles from './styles.module.css';
+import { useState } from 'react';
 import { CloseButton } from '../CloseButton';
+import { FeedbackTypeStep } from './Steps/FeedbackTypStep';
+
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/idea.svg';
+import thougthImageUrl from '../../assets/thought.svg';
+import styles from './styles.module.css';
+
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Ploblema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Imagem de um inseto',
+    },
+  },
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Imagem de uma lâmpada',
+    },
+  },
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: thougthImageUrl,
+      alt: 'Imagem de um balãoa de pensamento',
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm(){
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
   return(
     <div className={styles.widgetForm}>
       <header>
@@ -12,7 +47,11 @@ export function WidgetForm(){
       <CloseButton />
       </header>
 
-      <p>Hello Word!</p>
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+      ) : (
+        <p> ola mundo!</p>
+      )}
 
       <footer className={styles.footerWidgetForm}>
         {'Feito com ♥ pela '}

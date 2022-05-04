14792931b53b8215fa7bcfb2b591eb9414c0ce41
Ronald Assis <ronaldassis71@gmail.com>
2022-05-04 18:23:26 -0300

    fix: Finishing all Widget component

--- src/components/WidgetForm/Steps/FeedbackContentStep/index.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep/index.tsx
@@ -3,6 +3,7 @@ import { CloseButton } from '../../../CloseButton';
 import { FeedbackType, feedbackTypes } from '../../';
 
 import styles from '../../styles.module.css';
+import stylesContentStep from './styles.module.css';
 
 interface FeedbackContentStepProps {
   feedbackType: FeedbackType;
@@ -31,10 +32,25 @@ export function FeedbackContentStep({
            {feedbackTypeInfo.title}
         </span>
         <CloseButton />
-     </header>
-     <div className={styles.contentWidgetForm}>
-  
-    </div>
+      </header>
+      
+      <form className="my-4 w-full">
+        <textarea
+          className={stylesContentStep.textareaContentStep}
+          placeholder='Conte com detalhes o que esta acontecendo...'
+        />
+
+        <footer className='flex gap-2 mt-2'>
+          <button
+            type='submit'
+            className={`${stylesContentStep.sendButton} bg-brand-500`}
+          >
+            Enviar feedback
+          </button>
+          
+        </footer>
+      </form>
+    
   </>
   )
 }
